b54023d910ba10102252468faca81c0678b67efb
vishalmishradev <vishalmishra@medianv.com>
2026-02-26 11:43:20 +0530

    basics of ts

---
+++ Typescript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Helo ${person}, wlecome to my page.`;
+}
+const username = "shutpix";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ Typescript/src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string):string {
+    return `Hellow ${person}, wlecome to my page.`
+}
+
+const username = "shutpix";
+console.log(greet(username));
