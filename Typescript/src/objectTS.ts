bd47defc88f58da7a4ceb5bc551b82ec352aba7a
vishalmishradev <vishalmishra@medianv.com>
2026-02-26 13:43:14 +0530

    objects

---
+++ Typescript/src/objectTS.ts
@@ -0,0 +1,18 @@
+const chai ={
+    name:"masala chai",
+    price:20,
+    isHot:true,
+}
+
+let tea : {
+    name:String;
+    price:number;
+    isHot:boolean
+
+}
+
+tea = {
+    name:"Ginger Tea",
+    price:20,
+    isHot:true
+}
